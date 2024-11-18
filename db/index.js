5ee726afd03dbf5b495a664a6f9e5557fc6dc1f8
jesstheleftie <jessetang57@hotmail.com>
2024-11-17 22:46:41 -0800

    added seed file for products

---
+++ db/index.js
@@ -0,0 +1,16 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Successfully connected to MongoDB.");
+  })
+  .catch((e) => {
+    console.error("Connection error", e.message);
+  });
+
+const db = mongoose.connection;
+
+module.exports = db;
