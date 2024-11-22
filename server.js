a99f5b5fba20104ff534b5d710d8ef5e1ddf08f0
pedrocruz <JayJMarquez05@gmail.com>
2024-11-22 12:04:02 -0600

    Just commiting so my read me is out there

--- server.js
+++ server.js
@@ -1,33 +1,35 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const cors = require('cors');
-const express = require('express');
+const cors = require("cors");
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const testJWTRouter = require('./controllers/test-jwt');
-const usersRouter = require('./controllers/users');
-const profilesRouter = require('./controllers/profiles');
-const productsRouter = require('./controllers/products');
-const shoppingCartRouter = require('./controllers/shoppingCart');
-const shippingRouter = require('./controllers/shipping');
+const mongoose = require("mongoose");
+const testJWTRouter = require("./controllers/test-jwt");
+const usersRouter = require("./controllers/users");
+const profilesRouter = require("./controllers/profiles");
+const productsRouter = require("./controllers/products");
+const shoppingCartRouter = require("./controllers/shoppingCart");
+const shippingRouter = require("./controllers/shipping");
+const morgan = require(`morgan`);
 const PORT = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connection.on('connected', () => {
-    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
 
 // Routes go here
-app.use('/test-jwt', testJWTRouter);
-app.use('/users', usersRouter);
-app.use('/profiles', profilesRouter);
-app.use('/products', productsRouter);
-app.use('/cart', shoppingCartRouter);
-app.use('/shipping', shippingRouter);
+app.use("/test-jwt", testJWTRouter);
+app.use("/users", usersRouter);
+app.use("/profiles", profilesRouter);
+app.use("/products", productsRouter);
+app.use("/cart", shoppingCartRouter);
+app.use("/shipping", shippingRouter);
 
 app.listen(PORT, () => {
-    console.log('The express app is ready!');
+  console.log("The express app is ready!");
 });
